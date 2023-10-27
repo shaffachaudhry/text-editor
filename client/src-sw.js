5096449182d8c4d25ea575ba06e184679ba9bf30
shaffachaudhry <134459870+shaffachaudhry@users.noreply.github.com>
2023-10-27 18:16:05 -0500

    Update src-sw.js
    for asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,18 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  // checks if the request destination is one of 'style', 'script', or 'worker'.
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+    // Uses the Stale-While-Revalidate caching strategy for  the registered route
+  new StaleWhileRevalidate({
+    //Sets the name of the cache to be used for storage
+    cacheName: "asset-cache",
+    plugins: [
+      new CacheableResponsePlugin({
+        // Specify the HTTP statuses that should be cached 0 or 200
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
